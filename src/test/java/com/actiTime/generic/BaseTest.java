69fafbe7cb46a26fc523176aba4eaab7283aa387
BrahmendraJayaraju <81387382+BrahmendraJayaraju@users.noreply.github.com>
2026-02-08 13:41:43 -0500

    llllll

--- src/test/java/com/actiTime/generic/BaseTest.java
+++ src/test/java/com/actiTime/generic/BaseTest.java
@@ -14,47 +14,35 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public abstract class BaseTest implements AutoConstant {
 
     public WebDriver driver;
-
     @BeforeMethod
-    public void precondition() {
-
+    public void precondition() throws InterruptedException {
         String os = System.getProperty("os.name").toLowerCase();
-
         ChromeOptions options = new ChromeOptions();
 
         if (os.contains("mac")) {
-            // Mac: use your local chromedriver
             System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver");
-            // optional: run GUI Chrome
             options.addArguments("--start-maximized");
         } else {
-            // Linux (GitHub Actions VM): headless + WebDriverManager
+            // Linux CI
             WebDriverManager.chromedriver().setup();
-            options.addArguments("--headless");              // run headless
-            options.addArguments("--no-sandbox");            // Linux sandbox fix
-            options.addArguments("--disable-dev-shm-usage"); // memory fix
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
             options.addArguments("--disable-gpu");
             options.addArguments("--window-size=1920,1080");
-        }
+            options.addArguments("--remote-allow-origins=*");
 
-        // Common options for all platforms
-        options.addArguments("--disable-background-networking");
-        options.addArguments("--disable-background-timer-throttling");
-        options.addArguments("--disable-renderer-backgrounding");
-        options.addArguments("--disable-device-discovery-notifications");
-        options.addArguments("--disable-default-apps");
-        options.addArguments("--disable-sync");
-        options.addArguments("--disable-translate");
-        options.addArguments("--disable-features=VizDisplayCompositor");
-        options.addArguments("--disable-features=NetworkService");
-        options.addArguments("--disable-notifications");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--disable-infobars");
-        options.addArguments("--no-first-run");
-        options.addArguments("--remote-allow-origins=*");
+            // Add a short delay after page load for CI
+            Thread.sleep(5000); // 2 seconds
+        }
 
         driver = new ChromeDriver(options);
         driver.get("https://online.actitime.com/udel/login.do");
+
+        // Optional: another tiny wait to make sure login elements are present
+        if (!os.contains("mac")) {
+            Thread.sleep(5000);
+        }
     }
 
     @AfterMethod
