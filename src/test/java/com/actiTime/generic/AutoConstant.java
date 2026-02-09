40892106a3f023cca0019d7cb90d1a6c5afde0c4
BrahmendraJayaraju <81387382+BrahmendraJayaraju@users.noreply.github.com>
2026-02-08 22:22:46 -0500

    last

--- src/test/java/com/actiTime/generic/AutoConstant.java
+++ src/test/java/com/actiTime/generic/AutoConstant.java
@@ -4,10 +4,8 @@ public interface AutoConstant {
 
 	String Chrome_key = "webdriver.chrome.driver";
 	String Chrome_value = "./Driver/chromedriver";
-
 	String edge_key = "webdriver.edge.driver";
 	String edge_value = "./Driver/msedgedriver.exe";
-
 	String filepath = "./Data/input.xlsx";;
 
 }
